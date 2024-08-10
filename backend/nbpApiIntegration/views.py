61a5ff6a898c64222b1d21fbf6a5e5d98ec08af5
Jakub Rojewski <j.rojewski95@gmail.com>
2024-08-10 19:54:48 +0200

    Add example Gold Price request

--- backend/nbpApiIntegration/views.py
+++ backend/nbpApiIntegration/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import requests
+
+def make_get_request(url):
+  response = requests.get(url)
+  return response
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the nbpApiIntegration index.")
+    url = "http://api.nbp.pl/api/cenyzlota?format=json"
+    response = make_get_request(url)
+
+    if response.status_code == 200:
+        json = response.json()
+    else:
+        json = f"Request failed with status code {response.status_code}"
+    
+    return HttpResponse(json[0]['data'])
