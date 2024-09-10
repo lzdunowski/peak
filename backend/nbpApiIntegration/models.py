163815d5579aaa68282646bc0d3018c57b51c8eb
Jakub Rojewski <j.rojewski95@gmail.com>
2024-09-10 18:04:44 +0200

    Add Database and Currency related Models

--- backend/nbpApiIntegration/models.py
+++ backend/nbpApiIntegration/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Currency(models.Model):
+    name_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name_text
+
+class ExchangeRateOnDay(models.Model):
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    rate = models.FloatField(default=0.0)
+    date = models.DateTimeField("date")
+    def __str__(self):
+        return str(self.rate)
+    def was_checked_recently(self):
+        return self.date >= timezone.now() - datetime.timedelta(days=1)
+    
