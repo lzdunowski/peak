a58e2c2615d0ec0ef57941dbb1e72c2ad0934563
Lukasz Zdunowski <lukasz.zdunowski@gmail.com>
2024-12-03 21:44:47 +0100

    Add admin page and pgadmin service

---
+++ backend/peak/core/tests/test_admin.py
@@ -0,0 +1,44 @@
+"""
+Admin modifictaion.
+https://docs.djangoproject.com/en/5.1/ref/urlresolvers/
+https://docs.djangoproject.com/en/5.0/_modules/django/contrib/admin/sites/
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='uadmin@example.com',
+            password='examplepass123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='examplepass123',
+            name= 'Test user'
+        )
+
+    def test_user_list(self):
+        """Check if user is listed on page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user_load_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user_load_page(self):
+        url = reverse('admin:core_user_add')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
